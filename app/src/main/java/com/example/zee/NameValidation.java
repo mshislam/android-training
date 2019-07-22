f92440abcbde47178188b8746fb704a82d9bd488
ArwaAly <arwa.aly67@gmail.com>
2019-07-22 15:12:49 +0200

    remove validator files

--- app/src/main/java/com/example/zee/NameValidation.java
+++
@@ -1,8 +0,0 @@
-package com.example.zee;
-
-public class NameValidation extends RegexpValidator{
-    public NameValidation(String message) {
-        // will allow people with hyphens in his name or surname. Supports also unicode
-        super(message, "[\\p{L}- ]+");
-    }
-}
