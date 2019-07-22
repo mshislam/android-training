efade080a8c9d0749fae0156abba8e845278214d
zeinashabaka <zeinashabaka1@gmail.com>
2019-07-22 15:43:18 +0200

    create login fragment

--- app/src/main/java/com/example/zee/AuthenticationActivity.java
+++ app/src/main/java/com/example/zee/AuthenticationActivity.java
@@ -1,17 +1,30 @@
 package com.example.zee;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authentication_activity);
+        fragment();
     }
+
+    public void fragment(){
+
+        androidx.fragment.app.FragmentManager manager = getSupportFragmentManager();
+        androidx.fragment.app.FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_auth, new LoginFragment());
+        transaction.commit();
+
+
+    }
+
 }
