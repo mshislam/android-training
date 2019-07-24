aaec82d66a146d5713368b75f4f027d2dcef29a1
mshislam <mohamed_2slam@hotmail.com>
2019-07-24 16:43:26 +0200

    Text utilupdate functionality

--- app/src/main/java/com/example/zee/Util/TextUtil.java
+++ app/src/main/java/com/example/zee/Util/TextUtil.java
@@ -1,4 +1,4 @@
-package com.example.zee.Util;
+package com.example.zee.util;
 
 import android.widget.EditText;
 
@@ -6,37 +6,43 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class TextUtil  {
+public class TextUtil {
 
-    public static boolean isValid(EditText input){
-        String myText= input.getText().toString();
-        if(myText==null){
+    public static boolean isValid(EditText input) {
+        String myText = input.getText().toString();
+        if (myText == null) {
             return false;
         }
         //check if myText is phoneNumber
         String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(myText);
-        if(matcher.matches()){
-                 return true;
-                                }
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+        if (matcher.matches()) {
+            return true;
+        }
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
                 "[a-zA-Z0-9_+&*-]+)*@" +
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
                 "A-Z]{2,7}$";
 
         Pattern pat = Pattern.compile(emailRegex);
-        if(pat.matcher(myText).matches()){
+        if (pat.matcher(myText).matches()) {
             return true;
         }
         return false;
 
     }
+    public static boolean isEmpty(EditText et){
+        if(et.getText().toString().isEmpty()){
+            return false;
+        }else {
+            return true;
+        }
 
-
-
-
-
-
+    }
+    public static boolean isString(EditText et){
+        Pattern pattern=Pattern.compile("^[A-Za-z, ]++$");
+        return pattern.matcher(et.getText().toString()).matches();
+    }
 
 }
