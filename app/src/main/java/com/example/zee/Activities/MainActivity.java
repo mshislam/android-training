cd0acb26eb7e6efa23bb54685c1d07665022306e
mshislam <mohamed_2slam@hotmail.com>
2019-07-24 11:47:57 +0200

    Merging all classes

--- app/src/main/java/com/example/zee/Activities/MainActivity.java
+++ app/src/main/java/com/example/zee/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.zee.Fragments.LoginFragment;
 import com.example.zee.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragment();
+    }
+    public void fragment(){
+
+        androidx.fragment.app.FragmentManager manager = getSupportFragmentManager();
+        androidx.fragment.app.FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_auth, new LoginFragment()).addToBackStack(null);
+        transaction.commit();
+
     }
 }
