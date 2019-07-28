f66b9af9a3c6663b7ede57b82cb1ed24991d83cb
zeinashabaka <zeinashabaka1@gmail.com>
2019-07-28 13:33:29 +0200

    get extra layout

--- app/src/main/java/com/example/zee/Activities/CommentsActivity.java
+++ app/src/main/java/com/example/zee/Activities/CommentsActivity.java
@@ -1,10 +1,16 @@
 package com.example.zee.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.zee.R;
+import com.example.zee.Adapters.CommentsAdapter;
+
+import java.util.ArrayList;
 
 public class CommentsActivity extends AppCompatActivity {
 
@@ -12,6 +18,11 @@ public class CommentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
-        RecyclerView mRecyclerView-
+        RecyclerView mRecyclerView=findViewById(R.id.commentRecyclerActivity);
+        LinearLayoutManager mLinearLayoutManager=new LinearLayoutManager(CommentsActivity.this,RecyclerView.VERTICAL,false);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        ArrayList commentsList=new ArrayList<Object>();
+        CommentsAdapter commentsAdapter=new CommentsAdapter(CommentsActivity.this,commentsList);
+        mRecyclerView.setAdapter(commentsAdapter);
     }
 }
