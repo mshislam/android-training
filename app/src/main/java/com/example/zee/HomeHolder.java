14fa5d51a02d49b3800787ddcbf29d36417af92c
mshislam <mohamed_2slam@hotmail.com>
2019-07-28 11:24:25 +0200

    Recycler view add

---
+++ app/src/main/java/com/example/zee/HomeHolder.java
@@ -0,0 +1,12 @@
+package com.example.zee;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeHolder extends RecyclerView.ViewHolder {
+    public HomeHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
