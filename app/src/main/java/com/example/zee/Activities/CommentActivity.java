c86c8648cb897dab2a0531d934eb0ca83148010a
mshislam <mohamed_2slam@hotmail.com>
2019-07-28 14:58:58 +0200

    Final HomeActivity

---
+++ app/src/main/java/com/example/zee/Activities/CommentActivity.java
@@ -0,0 +1,16 @@
+package com.example.zee.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.zee.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
--- app/src/main/java/com/example/zee/Adapters/HomeAdapter.java
+++ app/src/main/java/com/example/zee/Adapters/HomeAdapter.java
@@ -9,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.zee.R;
 
 public class HomeAdapter extends RecyclerView.Adapter<HomeHolder> {
@@ -28,8 +30,10 @@ public HomeAdapter (Activity activity){
 
     @Override
     public void onBindViewHolder(@NonNull HomeHolder holder, int position) {
-        Glide.with(activity).load("https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjCkqyUs9fjAhUPAmMBHeS7ChgQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pngfind.com%2Fmpng%2FTomJxw_cara-mickey-mouse-png-rostro-de-mickey-mouse%2F&psig=AOvVaw12K11VnmegTb0wu18jtWjV&ust=1564395859300230").into(holder.icon);
-        Glide.with(activity).load("https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwie8aX7stfjAhUQmxQKHdHoAcMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.zajazdy.sk%2Fdovolenka-5-dnovy-zajazd-do-disneylandu-v-parizi-pariz-francuzsko-z731958%2F467161215%2F&psig=AOvVaw3XH6zwLRKYUAunksI4Rw1k&ust=1564395880481028").into(holder.imagePost);
+        Glide.with(activity).load("https://www.pngfind.com/pngs/m/59-593296_cara-mickey-mouse-png-rostro-de-mickey-mouse.png").into(holder.icon);
+        Glide.with(activity).load("https://pmcdeadline2.files.wordpress.com/2014/05/disneyland-tickets__140518205906.jpg?w=681&h=383&crop=1")
+                .apply(new RequestOptions().transform(new RoundedCorners(50))).into(holder.imagePost);
+
     }
 
     @Override
