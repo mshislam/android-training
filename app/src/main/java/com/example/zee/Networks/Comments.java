3c3ca677b32768c396cf564be8cee3e315018dfb
zeinashabaka <zeinashabaka1@gmail.com>
2019-07-29 11:56:02 +0200

    push some updates not final yet to zeina's branch

--- app/src/main/java/com/example/zee/Networks/Comments.java
+++ app/src/main/java/com/example/zee/Networks/Comments.java
@@ -1,8 +1,36 @@
 package com.example.zee.Networks;
 
-public class Comments {
+import android.content.Context;
 
-    public static void listComments() {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Comments {
+    public static void listComments(final Context context, final String LIST_COMMENTS , final String post_id){
+        RequestQueue mRequestQueue= Volley.newRequestQueue(context);
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, WebServiceConstants.Comments.API_LIST_COMMENTS, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
 
 
     }
--- app/src/main/java/com/example/zee/Networks/WebServiceConstants.java
+++ app/src/main/java/com/example/zee/Networks/WebServiceConstants.java
@@ -3,6 +3,8 @@ package com.example.zee.Networks;
 public class WebServiceConstants {
     private static String baseURL = "http://eventi-do1.mideastsoft.com/staging/api/v2/fdc/";
 
+    public static String API_TOKEN = "api_token";
+
     public static class Register {
         static String API_REGISTER = baseURL + "registration";
         public static String first_name = "first_name";
